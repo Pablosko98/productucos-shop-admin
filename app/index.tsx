f504b2b3aa139cf79fffd906b63756ab24a4bcce
Pablosko98 <boopablo100@gmail.com>
2026-02-16 22:22:20 +0000

    Fix user center bug

--- app/index.tsx
+++ app/index.tsx
@@ -50,21 +50,31 @@ const screenHeight = Dimensions.get('window').height;
     })()
   }, [])
   const handleShopSelect = (shop: Shop) => {
-    console.log('Selected shoppp:', shop);
     setSelectedShop(shop);
     cameraRef.current?.setCamera({
       centerCoordinate: shop.coords,
-      zoomLevel: 20,
+      zoomLevel: 14,
       animationDuration: 1000,
     });
   }
+  const [hasCenteredOnUser, setHasCenteredOnUser] = useState(false);
+  useEffect(() => {
+    if (userCoords && !hasCenteredOnUser) {
+      cameraRef.current?.setCamera({
+        centerCoordinate: userCoords,
+        zoomLevel: 14,
+        animationDuration: 1000,
+      });
+      setHasCenteredOnUser(true);
+    }
+  }, [userCoords, hasCenteredOnUser]);
   return (
     <View>
       <TouchableOpacity onPress={() => {
           if (userCoords) {
             cameraRef.current?.setCamera({
               centerCoordinate: userCoords,
-              zoomLevel: 16,
+              zoomLevel: 14,
               animationDuration: 1000,
             });
               setSelectedShop(null);
@@ -76,14 +86,7 @@ const screenHeight = Dimensions.get('window').height;
       style={{ height: screenHeight * 0.4, borderRadius: 10, overflow: 'hidden' }}
       mapStyle="https://api.maptiler.com/maps/streets-v2/style.json?key=9lKcz3dnis2TfSC9jJ8n"
     >
-      {userCoords && (
-        <Camera
-        ref={cameraRef}
-          zoomLevel={16}
-          animationDuration={1000}
-          followUserLocation={true}
-        />
-      )}
+      <Camera ref={cameraRef} />
       <UserLocation visible={true} onUpdate={(location) => {
     setUserCoords(location.coords
       ? [location.coords.longitude, location.coords.latitude]
@@ -110,11 +113,6 @@ const screenHeight = Dimensions.get('window').height;
       {shops.length > 0 && (
         shops.map((shop) => (
             <TouchableOpacity key={shop.id} onPress={() => {
-              // cameraRef.current?.setCamera({
-              //   centerCoordinate: shop.coords,
-              //   zoomLevel: 16,
-              //   animationDuration: 1000,
-              // });
               handleShopSelect(shop);
             }}>
           <Card className='mt-4' style={{ backgroundColor: selectedShop?.id === shop.id ? 'grey' : 'transparent', borderWidth: 2 }}>
